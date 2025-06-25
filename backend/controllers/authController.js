b4bb259b64a698047a9f9cfedffc014d95b2ecc2
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-06-25 15:27:37 +0530

    added otp verification

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User=require('../models/userModal')
-
+const generateOTP=require('../utils/generateOTP')
+const sendMail=require('../utils/sendMail')
 exports.signup = async (req, res) => {
   try {
     console.log('entered middle');
@@ -9,10 +10,10 @@ exports.signup = async (req, res) => {
  
     if (existing) return res.status(400).json({ message: 'User already exists' });
 
-    // const otp = generateOTP();
-    // await sendMail(email, otp);
+    const otp = generateOTP();
+    await sendMail(email, otp);
 
-    const user = new User({ email, name,password,mobileNo });
+    const user = new User({ email, name,password,mobileNo,otp });
     await user.save();
     console.log('saved');
     
@@ -20,5 +21,31 @@ exports.signup = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Signup failed', error: err.message });
+  }
+};
+exports.verifyOtp = async (req, res) => {
+
+  const { email, otp } = req.body || {}
+   if (!email || !otp) {
+    return res.status(400).json({ message: 'Email and OTP are required' });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    
+    // if (user.isVerified) return res.status(400).json({ message: 'User already verified' });
+
+    if (user.otp !== otp) return res.status(400).json({ message: 'Invalid OTP' });
+
+    user.isVerified = true;
+    user.otp = undefined; // Clear OTP
+    await user.save();
+
+    res.status(200).json({ message: 'OTP verified. User registered successfully!' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Verification failed', error: err.message });
   }
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router=express.Router()
-const {signup} =require('../controllers/authController')
+const {signup,verifyOtp} =require('../controllers/authController')
 
 router.post('/signup',signup, (req, res) => {
     console.log('enterd auth foloder');
     
   res.send('API is working 🚀');
 });
+router.post('/verify-otp', verifyOtp); // Step 2: Verify OTP and save
+
 
 module.exports=router
