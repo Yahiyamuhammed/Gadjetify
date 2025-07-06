f61e2eb9e1dfdf2507251101b12a18fa4f679754
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-06 15:49:22 +0530

    added sidebar

--- frontend/frontend-user/src/routes/adminRoutes.jsx
+++ frontend/frontend-user/src/routes/adminRoutes.jsx
@@ -1,9 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import AdminLogin from "../pages/admin/AdminLogin";
+import Sidebar from "../pages/admin/sideBar";
+// import { Sidebar } from "lucide-react";
+
+const AdminLayout = ({ children }) => {
+  return (
+    <>
+      <Sidebar />
+      <div className="ms-64 p-0">{children}</div>
+    </>
+  );
+};
+
 
 const AdminRoutes=()=>(
     <Routes>
-        <Route path="/" element={<AdminLogin />}/>
+        <Route path="/" element={
+            <AdminLayout>
+                <AdminLogin />
+
+            </AdminLayout>
+            }/>
     </Routes>
 )
 
