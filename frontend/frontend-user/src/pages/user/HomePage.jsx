cf5106ce6cf1daa5d7a89813d0c96390228a4119
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-20 13:26:11 +0530

    fix: add optional chaining at displaying products

--- frontend/frontend-user/src/pages/user/HomePage.jsx
+++ frontend/frontend-user/src/pages/user/HomePage.jsx
@@ -23,7 +23,7 @@ const HomePage = () => {
 
         {/* Products Grid */}
         <section className="grid gap-4 sm:gap-6 md:gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 justify-items-center px-4">
-          {products.products.length > 0 ? (
+          {products?.products.length > 0 ? (
             products.products.map((product) => (
               <ProductCard key={product._id} product={product} />
             ))
