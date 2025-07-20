52089eaa00b931e3e1c5867e2705da64077526f4
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-20 13:56:54 +0530

    fix: removed the fetching of auth user in useUserProducts

--- frontend/frontend-user/src/hooks/queries/useUserProductQueries.js
+++ frontend/frontend-user/src/hooks/queries/useUserProductQueries.js
@@ -9,11 +9,6 @@ export const useUserFetchProducts = (queryParams = {}) => {
     queryKey: ["products", queryParams],
     queryFn: async () => {
       const response = await api.get("/products", { params: queryParams });
-       if (response.data.user) {
-        // Save user data into React Query cache under "currentUser"
-        console.log('there is user in the session',response.data.user)
-        queryClient.setQueryData(["auth-user"], response.data.user);
-      }
       return response.data; // { products, totalCount, totalPages, currentPage }
     },
     keepPreviousData: true, // useful for paginated lists
