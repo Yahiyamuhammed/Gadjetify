0dffb602376ff60ba0b7a22d0ed0f180f8b66f83
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-21 20:06:29 +0530

    fix: add the middleware to user and brand routes

--- backend/routes/adminBrandRoutes.js
+++ backend/routes/adminBrandRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {addBrand,updateBrand,deleteBrand,getBrands,restoreBrand} = require('../controllers/adminBrandControllers');
+const adminAuth= require('../middlewares/adminAuth')
 
 router.post('/brands', addBrand);
-router.get('/brands', getBrands);
+router.get('/brands',adminAuth, getBrands);
 router.put('/brands/:id', updateBrand);
 router.patch('/brands/:id/delete', deleteBrand);
 router.patch('/brands/:id/restore', restoreBrand);
--- backend/routes/adminUserRoute.js
+++ backend/routes/adminUserRoute.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router=express.Router()
 
 const {getAllUsers,blockUnblockUser}=require('../controllers/adminUserController')
+const adminAuth= require('../middlewares/adminAuth')
 
-router.get('/users',getAllUsers)
+router.get('/users',adminAuth,getAllUsers)
 router.patch('/users/:id/block-toggle',blockUnblockUser)
 module.exports=router
