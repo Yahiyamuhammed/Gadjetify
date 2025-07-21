25fc8dbc4bd0ce27717f09c214167f82cc1f46ac
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-21 14:43:02 +0530

    fix: add the automatic logout when admin blocked the user

--- frontend/frontend-user/src/utils/api.js
+++ frontend/frontend-user/src/utils/api.js
@@ -1,8 +1,32 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:5000/api"; // update if your backend runs elsewhere
+const BASE_URL = "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // only if you are using cookies (e.g., JWT in cookies)
+  withCredentials: true,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const status = error?.response?.status;
+    const message = error?.response?.data?.message;
+
+    if (status === 403 && message?.toLowerCase().includes("blocked")) {
+      console.log("User is blocked");
+
+      try {
+        await api.post("/auth/signout");
+        window.location.reload();
+
+      } catch (err) {
+        console.warn("Error during signout:", err?.response?.data?.message || err.message);
+      }
+              // window.location.href = '/login';
+
+
+    }
+
+    return Promise.reject(error);
+  }
+);
