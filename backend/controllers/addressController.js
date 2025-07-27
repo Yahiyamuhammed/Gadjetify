7710e0050c8a32369371bccf22459507d6a227f7
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-27 17:15:55 +0530

    feat: created the controllers for address

---
+++ backend/controllers/addressController.js
@@ -0,0 +1,48 @@
+// controllers/addressController.js
+const addressHelper = require('../helpers/addressHelper')
+
+exports.addAddress = async (req, res) => {
+  try {
+    const userId = req.user._id
+    const address = await addressHelper.addAddress(userId, req.body)
+    res.status(201).json({ success: true, address })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
+
+exports.editAddress = async (req, res) => {
+  try {
+    const address = await addressHelper.editAddress(req.params.addressId, req.body, req.user._id)
+    res.json({ success: true, address })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
+
+exports.deleteAddress = async (req, res) => {
+  try {
+    await addressHelper.deleteAddress(req.params.addressId, req.user._id)
+    res.json({ success: true, message: 'Address deleted successfully' })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
+
+exports.setPrimaryAddress = async (req, res) => {
+  try {
+    const address = await addressHelper.setPrimaryAddress(req.user._id, req.params.addressId)
+    res.json({ success: true, address })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
+
+exports.getAddresses = async (req, res) => {
+  try {
+    const addresses = await addressHelper.getUserAddresses(req.user._id)
+    res.json({ success: true, addresses })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+}
