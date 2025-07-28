62215854585c1c2a6b2d88fab4812ddba60d2881
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-28 13:10:46 +0530

    fix: changed the fields to accept address type

--- backend/models/addressModal.js
+++ backend/models/addressModal.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const addressSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
   pincode: { type: String, required: true },
@@ -9,9 +9,10 @@ const addressSchema = new mongoose.Schema({
   address: { type: String, required: true },
   district: { type: String, required: true },
   state: { type: String, required: true },
-  landmark: String, // not required
-  alternatePhone: String, // not required
+  landmark: String,
+  alternatePhone: String,
   isPrimary: { type: Boolean, default: false },
+  addressType: {type: String,required: true,enum: ["home", "office"],default: "home"},
 });
 
 module.exports = mongoose.model("Address", addressSchema);
