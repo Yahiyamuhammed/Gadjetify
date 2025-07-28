90d7f687dd36e70fd821a04e270e2cdeef3a77ab
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-28 21:58:09 +0530

    fix: changed the import of functions in controller

--- backend/controllers/addressController.js
+++ backend/controllers/addressController.js
@@ -1,9 +1,9 @@
-const addressHelper = require('../helpers/addressHelper')
+const { addAddress, editAddress, deleteAddress, getUserAddresses } = require("../helpers/addressHelper")
 
 exports.addAddress = async (req, res) => {
   try {
     const userId = req.user._id
-    const address = await addressHelper.addAddress(userId, req.body)
+    const address = await addAddress(userId, req.body)
     res.status(201).json({ success: true, address })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
@@ -12,7 +12,7 @@ exports.addAddress = async (req, res) => {
 
 exports.editAddress = async (req, res) => {
   try {
-    const address = await addressHelper.editAddress(req.params.addressId, req.body, req.user._id)
+    const address = await editAddress(req.params.addressId, req.body, req.user._id)
     res.json({ success: true, address })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
@@ -21,7 +21,7 @@ exports.editAddress = async (req, res) => {
 
 exports.deleteAddress = async (req, res) => {
   try {
-    await addressHelper.deleteAddress(req.params.addressId, req.user._id)
+    await deleteAddress(req.params.addressId, req.user._id)
     res.json({ success: true, message: 'Address deleted successfully' })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
@@ -39,7 +39,7 @@ exports.setPrimaryAddress = async (req, res) => {
 
 exports.getAddresses = async (req, res) => {
   try {
-    const addresses = await addressHelper.getUserAddresses(req.user._id)
+    const addresses = await getUserAddresses(req.user._id)
     res.json({ success: true, addresses })
   } catch (error) {
     res.status(500).json({ success: false, message: error.message })
