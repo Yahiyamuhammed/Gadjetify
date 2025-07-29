92abe83093afb269d233676df65baaaef85d15fa
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-29 12:12:42 +0530

    feat: added the user vaient controller for user

--- backend/helpers/userVariantHelper.js
+++ backend/helpers/userVariantHelper.js
@@ -1,12 +1,8 @@
 const Variant = require('../models/variantModel');
 
-const fetchVariantsForProduct = async (productId) => {
+exports.fetchVariantsForProduct = async (productId) => {
   return await Variant.find({
     productId,
     isDeleted: { $ne: true },
   }).lean();
-};
-
-module.exports = {
-  fetchVariantsForProduct,
 };
