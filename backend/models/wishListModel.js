e749ede5443b9f0fc6afcd73ffdec74bb1570153
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-29 15:36:01 +0530

    feat: created the wishlis schema

---
+++ backend/models/wishListModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const wishlistSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true
+  },
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      variant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Variant',
+        required: true
+      }
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Wishlist', wishlistSchema);
