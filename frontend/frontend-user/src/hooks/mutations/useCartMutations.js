239361f4c1f725ba0b9aaef44614f9107817d648
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-30 01:15:48 +0530

    feat: add the cart mutations

---
+++ frontend/frontend-user/src/hooks/mutations/useCartMutations.js
@@ -0,0 +1,45 @@
+import { api } from "@/utils/api"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
+// Add to Cart
+export const useAddToCart = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async ({ productId, variantId }) => {
+      const res = await api.post("/cart", { productId, variantId })
+      return res.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"])
+      queryClient.invalidateQueries(["wishlist"]) // since item is removed from wishlist
+    },
+  })
+}
+
+// Update Quantity
+export const useUpdateCartQuantity = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async ({ variantId, quantity }) => {
+      const res = await api.patch(`/cart/${variantId}`, { quantity })
+      return res.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"])
+    },
+  })
+}
+
+// Remove from Cart
+export const useRemoveFromCart = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: async ({ variantId }) => {
+      const res = await api.delete(`/cart/${variantId}`)
+      return res.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["cart"])
+    },
+  })
+}
