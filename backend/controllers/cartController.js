f9461f2517c62c315c24f539e0648f0d4c029ce5
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-30 00:29:05 +0530

    fix: converted conroller to commonjs functions

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,35 +1,35 @@
-import * as cartHelper from "../helpers/cartHelper.js";
-
-export const addToCart = async (req, res) => {
+// import * as cartHelper from "../helpers/cartHelper.js";
+const {addToCart,getCart,updateQuantity,removeFromCart} =require('../helpers/cartHelper')
+exports.addToCart = async (req, res) => {
   try {
-    const response = await cartHelper.addToCart(req.user._id, req.body);
+    const response = await addToCart(req.user._id, req.body);
     res.json(response);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-export const getCart = async (req, res) => {
+exports.getCart = async (req, res) => {
   try {
-    const cart = await cartHelper.getCart(req.user._id);
+    const cart = await getCart(req.user._id);
     res.json(cart);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-export const updateCartQuantity = async (req, res) => {
+exports.updateCartQuantity = async (req, res) => {
   try {
-    const response = await cartHelper.updateQuantity(req.user._id, req.body);
+    const response = await updateQuantity(req.user._id, req.body);
     res.json(response);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-export const removeFromCart = async (req, res) => {
+exports.removeFromCart = async (req, res) => {
   try {
-    const response = await cartHelper.removeFromCart(req.user._id, req.body);
+    const response = await removeFromCart(req.user._id, req.body);
     res.json(response);
   } catch (err) {
     res.status(400).json({ error: err.message });
