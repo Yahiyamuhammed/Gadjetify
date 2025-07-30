6fe9403e4da2727bd173d6be2258bae46d954178
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-07-30 21:29:06 +0530

    feat: add the routed for getting orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const userAuth =require ("../middlewares/authMiddleware");
-const { placeOrder } = require('../controllers/orderController');
-
+const userAuth = require('../middlewares/authMiddleware');
+const { placeOrder, getUserOrders, getSingleOrder } = require('../controllers/orderController');
 
 router.post('/order', userAuth, placeOrder);
+router.get('/orders', userAuth, getUserOrders); 
+router.get('/order/:orderId', userAuth, getSingleOrder);
 
 module.exports = router;
