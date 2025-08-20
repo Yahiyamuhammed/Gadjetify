ab4b3711781f882e9bd2f784f24b98adf13d1933
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-08-20 10:13:06 +0530

    feat: wrapped the app in stripe

--- frontend/frontend-user/src/main.jsx
+++ frontend/frontend-user/src/main.jsx
@@ -1,31 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; 
-import { Toaster } from 'react-hot-toast';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
-
+const stripePromise = loadStripe(
+  "pk_test_51RxqyMAkWGOCuIh18GV1IIqWh3dPQcWTcxH2n3ISm4oofm6fSrKE9MTwHyAAvdNduPNasUm9fsbIDg7B435Cfstq00SOxwxDtx"
+);
 
 const queryClient = new QueryClient();
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
     <GoogleOAuthProvider clientId="162343963045-ja22a0buq2ht1t3dlsuicimehqi8f5or.apps.googleusercontent.com">
-
-    <QueryClientProvider client={queryClient}>
-     <BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        <App />
-        <Toaster />
-
-     </BrowserRouter>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+          <Toaster />
+        </BrowserRouter>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
