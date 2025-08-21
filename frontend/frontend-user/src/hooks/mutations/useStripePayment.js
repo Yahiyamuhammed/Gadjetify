8fc6bb01bd7781f24e5ae49ea68834f11882aefb
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-08-21 13:26:23 +0530

    feat: created the mutation functions

--- frontend/frontend-user/src/hooks/mutations/useStripePayment.js
+++ frontend/frontend-user/src/hooks/mutations/useStripePayment.js
@@ -1,12 +1,37 @@
-import { api } from "@/utils/api"
-import { useMutation } from "@tanstack/react-query"
-
+import { api } from "@/utils/api";
+import { useMutation } from "@tanstack/react-query";
 
 export const useStripePayment = () => {
   return useMutation({
     mutationFn: async ({ amount }) => {
-      const res = await api.post("/payment", { amount })
-      return res.data 
+      const res = await api.post("/payment", { amount });
+      return res.data;
     },
-  })
-}
+  });
+};
+export const usePaymentSuccess = () => {
+  return useMutation({
+    mutationFn: async ({ paymentId, orderId }) => {
+      const res = await api.post("/payment/success", { paymentId, orderId });
+      return res.data;
+    },
+  });
+};
+
+export const usePaymentFailure = () => {
+  return useMutation({
+    mutationFn: async ({ paymentId }) => {
+      const res = await api.post("/payment/failure", { paymentId });
+      return res.data;
+    },
+  });
+};
+
+export const useRetryPayment = () => {
+  return useMutation({
+    mutationFn: async ({ amount }) => {
+      const res = await api.post("/payment/retry", { amount });
+      return res.data;
+    },
+  });
+};
