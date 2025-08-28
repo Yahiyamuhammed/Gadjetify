ceece804e59e648e4b3864e82ea146eae2a89660
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-08-28 12:04:50 +0530

    feat: add the query for admin sales report

---
+++ frontend/frontend-user/src/hooks/queries/useReportQueries.js
@@ -0,0 +1,14 @@
+import { api } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+
+export const useSalesReport = ({ startDate, endDate, period, page, limit }) => {
+  return useQuery({
+    queryKey: ["salesReport", { startDate, endDate, period, page, limit }],
+    queryFn: async () => {
+      const res = await api.get("/admin/sales-report", {
+        params: { startDate, endDate, period, page, limit },
+      });
+      return res.data.data;
+    },
+  });
+};
