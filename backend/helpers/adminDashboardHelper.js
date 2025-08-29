3c053517d114d572da7ee5a42353d692971b4692
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-08-29 15:33:30 +0530

    feat: creted helper for summary

---
+++ backend/helpers/adminDashboardHelper.js
@@ -0,0 +1,29 @@
+const Order = require("../models/Order");
+
+exports.getSummaryData = async () => {
+  try {
+    const totalOrders = await Order.countDocuments();
+
+    const revenueAgg = await Order.aggregate([
+      { $match: { paymentStatus: "paid", status: { $ne: "Cancelled" } } },
+      { $group: { _id: null, totalRevenue: { $sum: "$finalTotal" } } }
+    ]);
+
+    const activeCustomers = await Order.distinct("userId");
+
+    const refunds = await Order.aggregate([
+      { $unwind: "$items" },
+      { $match: { "items.returnStatus": "returned" } },
+      { $count: "refunds" }
+    ]);
+
+    return {
+      totalRevenue: revenueAgg[0]?.totalRevenue || 0,
+      totalOrders,
+      activeCustomers: activeCustomers.length,
+      refunds: refunds[0]?.refunds || 0
+    };
+  } catch (error) {
+    throw error;
+  }
+};
