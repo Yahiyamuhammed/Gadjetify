f7997360f7f9cb7704e82618adfb7603e7a23bd0
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-08-29 18:22:56 +0530

    feat: add the controller for chart

--- backend/controllers/adminDashboardController.js
+++ backend/controllers/adminDashboardController.js
@@ -1,4 +1,7 @@
-const { getSummaryData } = require("../helpers/adminDashboardHelper");
+const {
+  getSummaryData,
+  getSalesReportHelper,
+} = require("../helpers/adminDashboardHelper");
 
 exports.getSummary = async (req, res) => {
   try {
@@ -7,5 +10,25 @@ exports.getSummary = async (req, res) => {
   } catch (error) {
     console.error("Error fetching summary:", error);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+exports.getSalesReport = async (req, res) => {
+  try {
+    const { startDate, endDate, period } = req.query;
+    if (!startDate || !endDate) {
+      return res
+        .status(400)
+        .json({ message: "startDate and endDate are required" });
+    }
+    const report = await getSalesReportHelper({ startDate, endDate, period });
+    return res.status(200).json({
+      status: 200,
+      message: "Sales report fetched successfully",
+      data: report,
+    });
+  } catch (err) {
+    console.error("Sales Report Error:", err);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
