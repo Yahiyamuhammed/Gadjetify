ab2bbe5268fd8664b67fee41d60540c50de1de51
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-02 21:55:49 +0530

    feat: added api for getting the cart count

--- backend/routes/userCartRoutes.js
+++ backend/routes/userCartRoutes.js
@@ -4,6 +4,7 @@ const {
   getCart,
   updateCartQuantity,
   removeFromCart,
+  getCartCount,
 } =require( "../controllers/cartController.js");
 
 const userAuth =require ("../middlewares/authMiddleware.js");
@@ -11,6 +12,7 @@ const userAuth =require ("../middlewares/authMiddleware.js");
 const router = express.Router();
 
 router.get("/cart", userAuth, getCart);
+router.get("/cart/count", userAuth, getCartCount);
 router.post("/cart", userAuth, addToCart);
 router.patch("/cart/:variantId", userAuth, updateCartQuantity);
 router.delete("/cart/:variantId", userAuth, removeFromCart);
