8b3c493b6cff7b2e30648157daab3402abb41829
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-02 10:16:39 +0530

    feat: created table columns

---
+++ frontend/frontend-user/src/components/admin/dashboard/tableColumns.js
@@ -0,0 +1,15 @@
+export const productColumns = [
+  { accessorKey: "product", header: "Product" },
+  { accessorKey: "variant", header: "Variant" },
+  { accessorKey: "brand", header: "Brand" },
+  { accessorKey: "unitsSold", header: "Units Sold" },
+  { accessorKey: "revenue", header: "Revenue" },
+  { accessorKey: "stockLeft", header: "Stock Left" },
+];
+
+export const brandColumns = [
+  { accessorKey: "brandName", header: "Brand" },
+  { accessorKey: "unitsSold", header: "Units Sold" },
+  { accessorKey: "revenue", header: "Revenue" },
+  { accessorKey: "numberOfProducts", header: "No. of Products" },
+];
