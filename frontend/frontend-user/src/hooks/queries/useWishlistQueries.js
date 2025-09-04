0cc987a4b6ec9713d064f08f33f0750faaf949f9
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-04 15:51:13 +0530

    fix: only run the wishlist when user is logged in

--- frontend/frontend-user/src/hooks/queries/useWishlistQueries.js
+++ frontend/frontend-user/src/hooks/queries/useWishlistQueries.js
@@ -1,12 +1,15 @@
 import { api } from "@/utils/api"
-import { useQuery } from "@tanstack/react-query"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 
 export const useFetchWishlist = () => {
+   const queryClient = useQueryClient();
+  const user = queryClient.getQueryData(["auth-user"]);
   return useQuery({
     queryKey: ['wishlist'],
     queryFn: async () => {
       const res = await api.get("/wishlist")
       return res.data.wishlist
-    }
+    },
+     enabled: !!user, 
   })
 }
