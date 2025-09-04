0aac64a09a625f1de0fd127e2b6cefdc9314a27c
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-04 10:53:50 +0530

    feat: created validation shema for otp

--- frontend/frontend-user/src/utils/validation/forgotOtpSchema.js
+++ frontend/frontend-user/src/utils/validation/forgotOtpSchema.js
@@ -1,9 +1,15 @@
 import * as yup from "yup";
 
-export const emailSchema = 
-  yup.object().shape({
-    email: yup
-      .string()
-      .email("Please enter a valid email address")
-      .required("Email is required"),
-  });
+export const emailSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter a valid email address")
+    .required("Email is required"),
+});
+
+export const otpSchema = yup.object().shape({
+  otp: yup
+    .string()
+    .matches(/^\d{6}$/, "OTP must be exactly 6 digits")
+    .required("OTP is required"),
+});
