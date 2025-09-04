5588a39f0b2d808dfc6303979daa061b2024ff10
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-04 15:46:30 +0530

    fix: removed the router.use of user auth

--- backend/routes/wishlistRoute.js
+++ backend/routes/wishlistRoute.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { getWishlist,toggleWishlistItem ,removeFromWishlist,clearWishlist} = require('../controllers/wishlistController');
 const userAuth = require('../middlewares/authMiddleware')
 
-router.use(userAuth);
 
-router.get('/wishlist', getWishlist);
-router.post('/wishlist/toggle', toggleWishlistItem);
-router.delete('/wishlist/remove', removeFromWishlist);
-router.delete('/wishlist/clear', clearWishlist);
+router.get('/wishlist', userAuth, getWishlist);
+router.post('/wishlist/toggle', userAuth, toggleWishlistItem);
+router.delete('/wishlist/remove', userAuth, removeFromWishlist);
+router.delete('/wishlist/clear', userAuth, clearWishlist);
+
 
 module.exports = router;
