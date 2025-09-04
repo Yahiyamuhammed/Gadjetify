f68ea8840ddab5dfbda6e458024f115d3dc33918
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-05 01:26:12 +0530

    feat: added the user auth for the user routs

--- backend/routes/user/index.js
+++ backend/routes/user/index.js
@@ -14,17 +14,20 @@ const paymentRoutes = require("./paymentRoutes");
 const couponRoutes = require("./couponRoutes");
 const ratingRoutes = require("./ratingRoutes");
 
+const userAuth = require('../../middlewares/authMiddleware')
+
+
 router.use("/auth", authRoutes);
-router.use("/address", userAddressRoutes);
+router.use("/address",userAuth, userAddressRoutes);
 router.use("/products", userProductRoutes);
-router.use("/wishlist", wishlistRoutes);
-router.use("/cart", cartRoutes);
-router.use("/order", orderRoutes);
-router.use("/wallet", walletRoutes);
-router.use("/profile", profileRoutes);
+router.use("/wishlist",userAuth,  wishlistRoutes);
+router.use("/cart",userAuth, cartRoutes);
+router.use("/order",userAuth, orderRoutes);
+router.use("/wallet",userAuth, walletRoutes);
+router.use("/profile",userAuth, profileRoutes);
 router.use("/brands", userBrandRoutes);
-router.use("/payment", paymentRoutes);
+router.use("/payment",userAuth, paymentRoutes);
 router.use("/coupon", couponRoutes);
-router.use("/rating", ratingRoutes);
+router.use("/rating",userAuth, ratingRoutes);
 
 module.exports = router;
