280e1538b1f506f6ccd6f1be87286cb7e4eaa269
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-12 21:00:07 +0530

    feat: add the route for it

--- backend/routes/user/paymentRoutes.js
+++ backend/routes/user/paymentRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createPayment, paymentSuccess, retryPayment,paymentFailed } = require("../../controllers/paymentController");
+const { createPayment, paymentSuccess, retryPayment,paymentFailed, verifyPayment } = require("../../controllers/paymentController");
 
 router.post("/", createPayment);
 router.post("/success", paymentSuccess);
 router.post("/failed", paymentFailed);
 router.post("/retry", retryPayment);
+router.get('/status',verifyPayment)
 
 module.exports = router;
