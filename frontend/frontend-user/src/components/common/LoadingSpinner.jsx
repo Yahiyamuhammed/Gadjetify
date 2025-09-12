3a50c644b0f2061da6fd4f7e0f6774e0a0c196a5
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-12 16:51:43 +0530

    feat: made the spinner for both component and full screen

--- frontend/frontend-user/src/components/common/LoadingSpinner.jsx
+++ frontend/frontend-user/src/components/common/LoadingSpinner.jsx
@@ -1,9 +1,16 @@
 import { Spinner } from "../ui/shadcn-io/spinner";
+const LoadingSpinner = ({ size = 44, fullscreen = false }) => {
+  if (fullscreen) {
+    return (
+      <div className="fixed inset-0 flex justify-center items-center bg-white/50 z-50">
+        <Spinner size={size} />
+      </div>
+    );
+  }
 
-const LoadingSpinner = ({ size = 44 }) => {
   return (
-    <div className="fixed inset-0 flex justify-center items-center">
-      <Spinner size={size} />
+    <div className="flex justify-center items-center p-4">
+      <Spinner size={16} />
     </div>
   );
 };
