64437cd78e469ce63b678e3c7782760ce9ba65c9
Yahiyamuhammed <yahiyamuhammed98@gmail.com>
2025-09-13 09:29:01 +0530

    feat: add the route to fetch wallet ballence

--- backend/routes/user/walletRoutes.js
+++ backend/routes/user/walletRoutes.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const router = express.Router();
-const { getUserWallet } = require('../../controllers/walletController');
+const { getUserWallet, getWalletBalanceController } = require('../../controllers/walletController');
 const checkBlockedUser = require('../../middlewares/checkBlockedUser');
 
 router.get('/', checkBlockedUser, getUserWallet);
+router.get("/balance", checkBlockedUser, getWalletBalanceController);
+
 
 module.exports = router;
